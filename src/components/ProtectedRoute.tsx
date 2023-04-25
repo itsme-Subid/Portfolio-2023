c04bdd9419a7bde5043eef7c1a58bf820da874b0
SUBID DAS <itsmesubid@gmail.com>
2023-04-26 01:24:15 +0530

    Minor Changes

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -5,8 +5,30 @@ import { useIdToken } from "react-firebase-hooks/auth";
 
 const ProtectedRoute = () => {
   const [user, loading, error] = useIdToken(auth);
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+  if (loading)
+    return (
+      <div
+        style={{
+          display: "grid",
+          placeContent: "center",
+          height: "100vh",
+        }}
+      >
+        Loading...
+      </div>
+    );
+  if (error)
+    return (
+      <div
+        style={{
+          display: "grid",
+          placeContent: "center",
+          height: "100vh",
+        }}
+      >
+        Error: {error.message}
+      </div>
+    );
   if (user?.email === "itsmesubid@gmail.com") return <Dashboard />;
   return <Navigate to="/login" />;
 };
